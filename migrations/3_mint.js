402b1236442ee05a87d28caee747ff9079d50b9d
Andy Gray <andy@whatsthescore.co.uk>
2018-04-25 15:43:38 +0100

    build and new migration

---
+++ migrations/3_mint.js
@@ -0,0 +1,30 @@
+const DART = artifacts.require('DART');
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraApikey = 'nbCbdzC6IG9CF6hmvAVQ';
+let mnemonic = require('../mnemonic');
+
+module.exports = function (deployer, network, accounts) {
+
+  console.log(`Minting a test token on DART contract to ${network}...`);
+
+  let _curatorAccount = accounts[0];
+
+  if (network === 'ropsten' || network === 'rinkeby') {
+    _curatorAccount = new HDWalletProvider(mnemonic, `https://${network}.infura.io/${infuraApikey}`, 0).getAddress();
+
+  }
+
+  if (network === 'live') {
+    let mnemonicLive = require('../mnemonic_live');
+    _curatorAccount = new HDWalletProvider(mnemonicLive, `https://mainnet.infura.io/${infuraApikey}`, 0).getAddress();
+  }
+
+  console.log(`_curatorAccount = ${_curatorAccount}`);
+
+  deployer
+    .then(() => DART.deployed())
+    .then((instance) => {
+      return instance.mint("XYZ", "TEST MINT 001");
+    });
+};
