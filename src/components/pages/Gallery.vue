fdf42daed644639edf2b474178e6a3b9ae6670dd
Andy Gray <andy@whatsthescore.co.uk>
2018-07-31 13:25:17 +0100

    better cards

--- src/components/pages/Gallery.vue
+++ src/components/pages/Gallery.vue
@@ -42,24 +42,27 @@
       <div class="col">
         <loading-spinner v-if="!assets || assets.length == 0"></loading-spinner>
 
-        <div class="card-columns">
-          <div class="card" v-for="dART, key in orderBy(limitBy(assets, 6), 'blocknumber', -1)" :key="key">
-            <div class="text-center">
-              <address-icon :ethAddress="dART.blockhash"></address-icon>
+
+        <div class="row">
+          <div class="col-2 mb-4" v-for="tokn, key in limitBy(orderBy(assets, 'blocknumber', -1), 100)" :key="key">
+            <div class="card">
+              <div class="text-center">
+                <address-icon :ethAddress="tokn.blockhash"></address-icon>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><span class="badge badge-primary float-right">{{ tokn.tokenId }}</span></h5>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  <small><samp>{{ tokn.blockhash }}</samp></small>
+                </li>
+              </ul>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  <small><samp>block: {{ tokn.blocknumber }}</samp></small>
+                </li>
+              </ul>
             </div>
-            <div class="card-body">
-              <h5 class="card-title"><span class="badge badge-primary float-right">{{ dART.tokenId }}</span></h5>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                <small><samp>{{ dART.blockhash }}</samp></small>
-              </li>
-            </ul>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">
-                <small><samp>block: {{ dART.blocknumber }}</samp></small>
-              </li>
-            </ul>
           </div>
         </div>
 
@@ -70,12 +73,12 @@
 
 <script>
 
-  import {mapGetters, mapState} from 'vuex';
+  import { mapGetters, mapState } from 'vuex';
   import LoadingSpinner from '../ui-controls/LoadingSpinner.vue';
   import ClickableAddress from '../ui-controls/ClickableAddress';
   import AddressIcon from '../ui-controls/AddressIcon';
   import * as actions from '../../store/actions';
-  import CurrentNetwork from "../ui-controls/CurrentNetwork.vue";
+  import CurrentNetwork from '../ui-controls/CurrentNetwork.vue';
 
   export default {
     name: 'gallery',
