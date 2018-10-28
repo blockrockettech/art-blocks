760780ba0ee3612d7b774ee1927f873ad59eddaf
James Morgan <james.morgan.e@googlemail.com>
2018-10-28 15:26:56 +0000

    Fixing bad flatten

--- truffle_flatten.sh
+++ truffle_flatten.sh
@@ -2,6 +2,7 @@
 
 node ./node_modules/.bin/truffle-flattener ./contracts/Migrations.sol > ./contracts-flat/Migrations.sol;
 
-node ./node_modules/.bin/truffle-flattener ./contracts/InterfaceToken.sol > ./contracts-flat/InterfaceToken.sol;
+node ./node_modules/.bin/truffle-flattener ./contracts/v1/InterfaceToken.sol > ./contracts-flat/InterfaceToken.sol;
 node ./node_modules/.bin/truffle-flattener ./contracts/v1/SimpleArtistContract.sol > ./contracts-flat/SimpleArtistContract.sol;
+
 node ./node_modules/.bin/truffle-flattener ./contracts/v2/SimpleArtistContractV2.sol > ./contracts-flat/SimpleArtistContractV2.sol;
